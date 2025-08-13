7dd3c8866b3a4c1ecea9afd158bd4ea3c2278a9c
unknown <felipesantossnx@gmail.com>
2025-08-13 10:24:27 -0300

    criação de rota e correção de models

--- Frontend/src/app/models/endereco.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Endereco{
-    tipo: string,
-    logradouro: string,
-    numero: number,
-    complemento: string,
-    cep: string,
-    cidade: string,
-    estado: string
-}
---
+++ Frontend/src/app/shared/models/cliente.model.ts
@@ -0,0 +1,13 @@
+import { Endereco } from "./endereco.model";
+
+export class Cliente {
+
+    constructor(
+        public nome: string,
+        public email: string = "",
+        public cpf: string = "",
+        public endereco: Endereco,
+        public telefone: string = "",
+        public salario: number = 0.0
+    ){}
+}

793bafb68b7aed81f2d29212276d89398e51b571
Luiz Bussolo <bussolo@ufpr.br>
2025-08-18 17:18:10 -0300

    Adicionado novo component atualizar-cliente

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AutocadastroComponent } from './pages/autocadastro';
+import { AtualizarClienteComponent } from './pages/atualizar-cliente/atualizar-cliente.component';
 
 export const routes: Routes = [
-    { path: 'autocadastro', component: AutocadastroComponent}
+    { path: 'autocadastro', component: AutocadastroComponent},
+    { path: 'atualizarcliente', component: AtualizarClienteComponent}
 ];

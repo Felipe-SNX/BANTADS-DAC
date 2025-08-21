256eeed5a1ab05cee843fbf391ea3fe0dd76fdee
unknown <felipesantossnx@gmail.com>
2025-08-21 10:13:02 -0300

    Melhorias na tela

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AutocadastroComponent } from './pages/autocadastro';
 import { AtualizarClienteComponent } from './pages/atualizar-cliente/atualizar-cliente.component';
+import { ConsultaExtratoComponent } from './pages/consulta-extrato/consulta-extrato.component';
 
 export const routes: Routes = [
     { path: 'autocadastro', component: AutocadastroComponent},
-    { path: 'atualizarcliente', component: AtualizarClienteComponent}
+    { path: 'atualizarcliente', component: AtualizarClienteComponent},
+    { path: 'consultaExtrato', component: ConsultaExtratoComponent}
 ];
---
+++ Frontend/src/app/pages/consulta-extrato/consulta-extrato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-extrato',
+  standalone: true,
+  imports: [],
+  templateUrl: './consulta-extrato.component.html',
+  styleUrl: './consulta-extrato.component.css'
+})
+export class ConsultaExtratoComponent {
+
+}

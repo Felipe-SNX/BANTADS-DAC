f28fed6c724dcd517817351f3c0ea1e03a5a1c3d
Luiz Bussolo <bussolo@ufpr.br>
2025-09-14 17:26:48 -0300

    Implementa tela de dashboard para admin apresentando gerentes e saldos

--- Frontend/src/app/pages/tela-inicial-admin/tela-inicial-admin.component.ts
+++ Frontend/src/app/pages/tela-inicial-admin/tela-inicial-admin.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+
+interface AdminDashboard {
+  id: number;
+  nome: string;
+  email: string;
+  totalClientes: number;
+  saldoPositivo: number;
+  saldoNegativo: number;
+}
 
 @Component({
   selector: 'app-tela-inicial-admin',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [
+    SidebarComponent,
+    CommonModule
+  ],
   templateUrl: './tela-inicial-admin.component.html',
   styleUrl: './tela-inicial-admin.component.css'
 })
-export class TelaInicialAdminComponent {
 
+export class TelaInicialAdminComponent implements OnInit {
+  AdminDashboard: AdminDashboard[] = [];
+
+  ngOnInit(): void {
+    const gerentes  = JSON.parse(localStorage.getItem('gerentes') || '[]');
+    const clientes = JSON.parse(localStorage.getItem('clientes') || '[]');
+    const contas = JSON.parse(localStorage.getItem('contas') || '[]');
+    
+    this.AdminDashboard = gerentes.map((gerente: any) => {
+      const clientesDoGerente = gerente.clientes || [];
+      const totalClientes = clientesDoGerente.length;
+
+      const contasDoGerente = contas
+      .filter((conta:any) => clientesDoGerente.some((cliente: any) => cliente.id === conta.cliente.id));
+    
+      const saldoPositivo = contasDoGerente
+      .filter((conta: any) => conta.saldo >= 0)
+      .reduce((acc: number, conta: any) => acc + conta.saldo, 0);
+
+      const saldoNegativo = contasDoGerente
+      .filter((conta: any) => conta.saldo < 0)
+      .reduce((acc: number, conta: any) => acc + conta.saldo, 0); 
+
+      return {
+        id: gerente.id,
+        nome: gerente.nome,
+        email: gerente.email,
+        totalClientes: totalClientes,
+        saldoPositivo: saldoPositivo,
+        saldoNegativo: saldoNegativo
+      };
+    });
+
+    // Ordena os gerentes pelo maior saldo positivo
+    //this.gerentesDashboard.sort((a, b) => b.saldoPositivo - a.saldoNegativo);
+
+  }
 }

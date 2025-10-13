870f564bcfb9af0144f4f28cbe9d8ec29b55d5a5
unknown <felipesantossnx@gmail.com>
2025-10-13 10:11:35 -0300

    inicio microsserviço

--- Backend/ms-conta/src/main/java/com/bantads/contaservice/BantadsApplication.java
+++ Backend/ms-conta/src/main/java/com/bantads/contaservice/BantadsApplication.java
@@ -1,4 +1,4 @@
-package com.dac.bantads;
+package com.bantads.contaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Backend/ms-conta/src/test/java/com/bantads/contaservice/BantadsApplicationTests.java
+++ Backend/ms-conta/src/test/java/com/bantads/contaservice/BantadsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dac.bantads;
+package com.bantads.contaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

190cf68898e434532f5b105f9b6ca8099773c978
unknown <felipesantossnx@gmail.com>
2025-10-21 11:05:34 -0300

    alterações no api-gateway

--- Backend/api-gateway/src/api/routes/contas.routes.js
+++ Backend/api-gateway/src/api/routes/contas.routes.js
@@ -1,27 +1,31 @@
 const { Router } = require('express');
 const verifyToken = require('../../middlewares/auth.middleware.js');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const router = Router();
-const contasServiceProxy = httpProxy('http://localhost:5002');
+const contasServiceProxy = createProxyMiddleware({
+    target: process.env.MS_CONTA_URL,
+    changeOrigin: true,
+});
 
-router.post('/:numero/saldo', verifyToken, (req, res, next) => {
-    contasServiceProxy(req,res,next); 
+router.get('/contas/:numero/saldo', (req, res, next) => {
+    contasServiceProxy(req,res,next);
 });
 
 router.post('/:numero/depositar', verifyToken, (req, res, next) => {
-    contasServiceProxy(req,res,next); 
+    contasServiceProxy(req,res,next);
 });
 
 router.post('/:numero/sacar', verifyToken, (req, res, next) => {
-    contasServiceProxy(req,res,next); 
+    contasServiceProxy(req,res,next);
 });
 
 router.post('/:numero/transferir', verifyToken, (req, res, next) => {
-    contasServiceProxy(req,res,next); 
+    contasServiceProxy(req,res,next);
 });
 
-router.post('/:numero/extrato', verifyToken, (req, res, next) => {
-    contasServiceProxy(req,res,next); 
+router.get('/:numero/extrato', verifyToken, (req, res, next) => {
+    contasServiceProxy(req,res,next);
 });
 
 module.exports = router;

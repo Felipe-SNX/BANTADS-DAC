669c385ad70c3fc1dd73b06d7700c1e7c6ded06e
FELIPE EDUARDO DOS SANTOS <fesantos@mppr.mp.pr>
2025-10-22 16:32:52 -0300

    adição de construtor privado

--- Backend/ms-conta/src/main/java/com/bantads/msconta/config/rabbitmq/RabbitMQConstantes.java
+++ Backend/ms-conta/src/main/java/com/bantads/msconta/config/rabbitmq/RabbitMQConstantes.java
@@ -2,6 +2,9 @@ package com.bantads.msconta.config.rabbitmq;
 
 public class RabbitMQConstantes {
 
+    private RabbitMQConstantes() {
+    }
+
     public static final String NOME_EXCHANGE = "bantads.eventos";
     public static final String FILA_CONTA_SYNC = "conta.sync";
     public static final String ROUTING_KEY = "conta.movimentacao.realizada";

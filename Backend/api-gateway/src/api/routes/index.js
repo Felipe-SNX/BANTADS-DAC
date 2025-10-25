f76a88c0313008e616929e3a5ef8bcf188112e0b
unknown <felipesantossnx@gmail.com>
2025-10-25 13:34:22 -0300

    ajustes para funcionar ms-gerente via api-gateway

--- Backend/api-gateway/src/api/routes/index.js
+++ Backend/api-gateway/src/api/routes/index.js
@@ -1,10 +1,6 @@
 const { Router } = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// Importa os roteadores de cada domínio
-const clientesRoutes = require('./clientes.routes.js');
-const gerentesRoutes = require('./gerentes.routes.js');
-
 const router = Router();
 
 const contasServiceProxy = createProxyMiddleware({
@@ -19,7 +15,7 @@ const contasServiceProxy = createProxyMiddleware({
 });
 
 const clientesServiceProxy = createProxyMiddleware({
-    target: process.env.MS_CONTA_URL,
+    target: process.env.MS_CLIENTE_URL,
     changeOrigin: true,
     logLevel: 'debug',
     pathRewrite: (path, req) => {
@@ -34,14 +30,19 @@ const gerentesServiceProxy = createProxyMiddleware({
     changeOrigin: true,
     logLevel: 'debug',
     pathRewrite: (path, req) => {
+        //Isso está aqui, pois nas rotas sem parâmetros estava dando erro por causa da barra extra
+        if (path === '/') {
+            console.log(`[Proxy PathRewrite] Path (raiz) reescrito para: /gerentes`);
+            return '/gerentes';
+        }
         const newPath = '/gerentes' + path;
-        console.log(`[Proxy PathRewrite] Path reescrito para: ${newPath}`);
+        console.log(`[Proxy PathRewrite] Path (sub-rota) reescrito para: ${newPath}`);
         return newPath;
     },
 });
 
+router.use('/gerentes', gerentesServiceProxy);
 router.use('/clientes', clientesServiceProxy);
 router.use('/contas', contasServiceProxy);
-router.use('/gerentes', gerentesServiceProxy);
 
 module.exports = router;
